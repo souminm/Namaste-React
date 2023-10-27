207bbeebd70be8bf48bf4b0d32ded0b2dba1fd52
soumin mohanty <soummohanty@deloitte.com>
2023-10-27 12:07:17 +0530

    Episode 1 Namaste React

---
+++ app.js
@@ -0,0 +1,14 @@
+const heading =  React.createElement('h1',{id:"heading"},"Hello world from React");
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" },[
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag"),
+      ]),
+      React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag"),
+      ])
+  ]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
+// root.render(heading);
