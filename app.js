f7290b53dcd059ee6d59ade9118af62e3c265674
soumin mohanty <soummohanty@deloitte.com>
2023-10-29 18:38:38 +0530

    Episode No 3 Namaste React

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading =  React.createElement('h1',{id:"heading"},"Hello world from React");
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" },[
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-      ]),
-      React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-      ])
-  ]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+
+const parent = <span>Hey hi!!</span>
+  const Title= () =>{return <h1>Hello world from React
+    {parent}  
+    {/* <HeadingComponent/> */}
+  </h1>
+  
+}
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(parent);
 // root.render(heading);
+
+//React element
+//JSX heading
+//JSX(Transpilesbefore it reaches the JS) - PARCEL - Babel
+//JSX => Babel transpiles it to React.createElement => ReactElement ->JS Object -> HTMLElement(render)
+const heading = <h1 className="head">Namaste React using JSX!</h1>;
+
+const HeadingComponent = () =>(
+  <div className="container">
+    <h1>Namaste React functional component</h1>
+     <Title/>
+  </div> 
+ 
+);  
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(<HeadingComponent/>)
+
+//React Component
+//class based component - old way of writing code
+//functional Component - new way of writing code
+
